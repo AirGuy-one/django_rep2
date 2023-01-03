dfa6e6e7d96d9a90666ca43f606a27c1a3b83e83
AirGuy-one <airguy-one@yandex.ru>
2023-01-03 20:59:59 +0300

    Now, when the manager finishes changing the order, he will return back to the orders page

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.http import HttpResponseRedirect
 
 from .models import Product
 from .models import ProductCategory
@@ -130,3 +131,10 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         ProductsInOrderInline
     ]
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if "next" in request.GET:
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
