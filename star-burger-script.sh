61824a6c2de4fe4f4da06f193c1e1f8e25d74d05
AirGuy-one <airguy-one@yandex.ru>
2023-04-13 01:00:02 +0300

    Add to script the sending data about deploying to the Rollbar

--- star-burger-script.sh
+++ star-burger-script.sh
@@ -6,10 +6,22 @@ set -e -o pipefail
 cd /opt/django_rep2/
 source venv/bin/activate
 git pull
-pip install -r requirements.txt
-npm ci --dev
+#pip install -r requirements.txt
+#npm ci --dev
 python3 manage.py makemigrations
 python3 manage.py migrate
 echo "yes" | python3 manage.py collectstatic
 systemctl reload star-burger
-echo "The script has been successfully completed!"
+
+access_token="71fcf0f288e641d9ba832a19d635b2b5"
+revision="git SHA"
+local_username="root server user"
+comment="deploy was successfully"
+
+curl -X POST \
+     -H "X-Rollbar-Access-Token: $access_token" \
+     -H "Content-Type: application/json" \
+     -d '{"environment": "production", "revision": "'"$revision"'", "local_username": "'"$local_username"'", "comment": "'"$comment"'"}' \
+     https://api.rollbar.com/api/1/deploy
+
+echo -e "\nThe script has been successfully completed!"
