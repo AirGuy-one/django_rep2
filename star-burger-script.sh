ed30ce218ebc6250ce96c2a242a3cc8687c0b81c
AirGuy-one <airguy-one@yandex.ru>
2023-07-10 01:33:57 +0300

    Update script for uploading updates to the server

--- star-burger-script.sh
+++ star-burger-script.sh
@@ -7,13 +7,13 @@ cd /opt/django_rep2/
 git pull
 cd infra/
 sudo docker-compose -f docker-compose-prod.yml up -d --build
+ACCESS_TOKEN=$(docker ps --filter "publish=8000" --format "{{.ID}}" | xargs docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' | grep ACCESS_TOKEN | cut -d '=' -f 2)
 
-access_token="${ACCESS_TOKEN}"
 revision=$(git rev-parse HEAD)
 local_username="root server user"
 
 curl -X POST \
-     -H "X-Rollbar-Access-Token: $access_token" \
+     -H "X-Rollbar-Access-Token: $ACCESS_TOKEN" \
      -H "Content-Type: application/json" \
      -d '{"environment": "production", "revision": "'"$revision"'", "local_username": "'"$local_username"'", "comment": "'"$comment"'"}' \
      https://api.rollbar.com/api/1/deploy
